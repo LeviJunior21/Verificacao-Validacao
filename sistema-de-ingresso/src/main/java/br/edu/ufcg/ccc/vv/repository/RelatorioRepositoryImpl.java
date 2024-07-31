3bbadb472046974bcd63175bd5ff8b1ceb007fd9
franciscodantas <francisco.antonio.sousa@ccc.ufcg.edu.br>
2024-07-31 09:15:26 -0300

    passando nos testes

--- sistema-de-ingresso/src/main/java/br/edu/ufcg/ccc/vv/repository/RelatorioRepositoryImpl.java
+++ sistema-de-ingresso/src/main/java/br/edu/ufcg/ccc/vv/repository/RelatorioRepositoryImpl.java
@@ -2,27 +2,32 @@ package br.edu.ufcg.ccc.vv.repository;
 
 import br.edu.ufcg.ccc.vv.models.RelatorioModel;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
 public class RelatorioRepositoryImpl implements RelatorioRepository {
+    private final HashMap<Integer, RelatorioModel> database = new HashMap<>();
+
     @Override
     public RelatorioModel save(RelatorioModel relatorio) {
-        return null;
+        database.put(relatorio.getNumIngresso(), relatorio);
+        return relatorio;
     }
 
     @Override
     public Optional<RelatorioModel> findById(Integer id) {
-        return Optional.empty();
+        return Optional.ofNullable(database.get(id));
     }
 
     @Override
     public List<RelatorioModel> findAll() {
-        return List.of();
+        return new ArrayList<>(database.values());
     }
 
     @Override
     public void deleteById(Integer id) {
-
+        database.remove(id);
     }
 }
