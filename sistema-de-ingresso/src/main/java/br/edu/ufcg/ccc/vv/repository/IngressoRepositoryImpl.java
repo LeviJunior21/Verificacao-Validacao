e6e0e0b2a798e13ffbd6e8c49cee2aefc1e55ae9
franciscodantas <francisco.antonio.sousa@ccc.ufcg.edu.br>
2024-07-31 09:08:48 -0300

    passando nos testes

--- sistema-de-ingresso/src/main/java/br/edu/ufcg/ccc/vv/repository/IngressoRepositoryImpl.java
+++ sistema-de-ingresso/src/main/java/br/edu/ufcg/ccc/vv/repository/IngressoRepositoryImpl.java
@@ -2,27 +2,32 @@ package br.edu.ufcg.ccc.vv.repository;
 
 import br.edu.ufcg.ccc.vv.models.IngressoModel;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
 public class IngressoRepositoryImpl implements IngressoRepository {
+    private final HashMap<Long, IngressoModel> database = new HashMap<>();
+
     @Override
     public IngressoModel save(IngressoModel ingresso) {
-        return null;
+        database.put(ingresso.getId(), ingresso);
+        return ingresso;
     }
 
     @Override
     public Optional<IngressoModel> findById(Long id) {
-        return Optional.empty();
+        return Optional.ofNullable(database.get(id));
     }
 
     @Override
     public List<IngressoModel> findAll() {
-        return List.of();
+        return new ArrayList<>(database.values());
     }
 
     @Override
     public void deleteById(Long id) {
-
+        database.remove(id);
     }
 }
