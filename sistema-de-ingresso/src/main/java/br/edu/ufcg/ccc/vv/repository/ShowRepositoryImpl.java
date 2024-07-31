790097aaff82fa72783109d12f63c08a69a56ad6
franciscodantas <francisco.antonio.sousa@ccc.ufcg.edu.br>
2024-07-31 09:19:30 -0300

    passando nos testes

--- sistema-de-ingresso/src/main/java/br/edu/ufcg/ccc/vv/repository/ShowRepositoryImpl.java
+++ sistema-de-ingresso/src/main/java/br/edu/ufcg/ccc/vv/repository/ShowRepositoryImpl.java
@@ -2,28 +2,36 @@ package br.edu.ufcg.ccc.vv.repository;
 
 import br.edu.ufcg.ccc.vv.models.ShowModel;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class ShowRepositoryImpl implements ShowRepository {
+    private final Map<String, ShowModel> database = new HashMap<>();
+
     @Override
     public ShowModel save(ShowModel show) {
-        return null;
+        String key = generateKey(show.getData(), show.getArtista());
+        database.put(key, show);
+        return show;
     }
 
     @Override
     public Optional<ShowModel> findById(Date id, String artista) {
-        return Optional.empty();
+        String key = generateKey(id, artista);
+        return Optional.ofNullable(database.get(key));
     }
 
     @Override
     public List<ShowModel> findAll() {
-        return List.of();
+        return new ArrayList<>(database.values());
     }
 
     @Override
     public void deleteById(Date id, String artista) {
+        String key = generateKey(id, artista);
+        database.remove(key);
+    }
 
+    private String generateKey(Date data, String artista) {
+        return data.toString() + "|" + artista;
     }
 }
