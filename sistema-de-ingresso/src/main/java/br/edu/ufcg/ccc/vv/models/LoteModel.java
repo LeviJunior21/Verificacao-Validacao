68a174c66dc662f12e8c5f38d5ba6d8378501740
franciscodantas <francisco.antonio.sousa@ccc.ufcg.edu.br>
2024-07-31 08:52:10 -0300

    criando o modelo lote

---
+++ sistema-de-ingresso/src/main/java/br/edu/ufcg/ccc/vv/models/LoteModel.java
@@ -0,0 +1,9 @@
+package br.edu.ufcg.ccc.vv.models;
+
+import java.util.List;
+
+public class LoteModel {
+    private Long id;
+    private List<IngressoModel> ingressos;
+    private Double desconto;
+}
---
+++ sistema-de-ingresso/src/test/java/br/edu/ufcg/ccc/vv/models/LoteModelTest.java
@@ -0,0 +1,17 @@
+package br.edu.ufcg.ccc.vv.models;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoteModelTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
