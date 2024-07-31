6d09d3cf2dfb07ae986858817994d1d2e8f076e4
franciscodantas <francisco.antonio.sousa@ccc.ufcg.edu.br>
2024-07-31 08:55:59 -0300

    passando nos testes

--- sistema-de-ingresso/src/main/java/br/edu/ufcg/ccc/vv/models/LoteModel.java
+++ sistema-de-ingresso/src/main/java/br/edu/ufcg/ccc/vv/models/LoteModel.java
@@ -6,4 +6,28 @@ public class LoteModel {
     private Long id;
     private List<IngressoModel> ingressos;
     private Double desconto;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<IngressoModel> getIngressos() {
+        return ingressos;
+    }
+
+    public void setIngressos(List<IngressoModel> ingressos) {
+        this.ingressos = ingressos;
+    }
+
+    public Double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(Double desconto) {
+        this.desconto = desconto;
+    }
 }
