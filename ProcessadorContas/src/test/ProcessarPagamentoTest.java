9236c61b2f75b15a67ccead20d8884181ff9bc7d
LeviJunior21 <levi.pereira.junior@ccc.ufcg.edu.br>
2024-07-31 22:26:33 -0300

    Quando realizamos pagamento boleto pago antes do vencimentp

--- ProcessadorContas/src/test/ProcessarPagamentoTest.java
+++ ProcessadorContas/src/test/ProcessarPagamentoTest.java
@@ -24,7 +24,7 @@ public class ProcessarPagamentoTest {
 
     @Test
     void testRealizarPagamentoBoletoPagoAposVencimento() {
-        Date dataVencimento = new Date(dataPagamento.getTime() - (1000 * 60 * 60 * 24 * 1)); // 1 dia atrás
+        Date dataVencimento = new Date(dataPagamento.getTime() - (1000 * 60 * 60 * 24 * 1));
         Conta conta = new Conta("001", dataVencimento, 100.00, TipoPagamento.BOLETO);
         
         Pagamento pagamento = processadorContas.realizarPagamento(conta, dataPagamento);
@@ -39,6 +39,20 @@ public class ProcessarPagamentoTest {
         double margemErro = 0.01;
         
         assertEquals(valorEsperado, valorPago, margemErro, "O valor pago deve ter 10% de acréscimo");
+        assertEquals(TipoPagamento.BOLETO, pagamento.getTipoPagamento(), "O tipo de pagamento deve ser BOLETO");
+    }
+
+    @Test
+    void testRealizarPagamentoBoletoPagoAntesDoVencimento() {
+        Date dataVencimento = new Date(dataPagamento.getTime() + (1000 * 60 * 60 * 24 * 1));
+        Conta conta = new Conta("001", dataVencimento, 100.00, TipoPagamento.BOLETO);
+        
+        Pagamento pagamento = processadorContas.realizarPagamento(conta, dataPagamento);
+        
+        assertNotNull(pagamento, "Pagamento não deve ser nulo");
+        double valorEsperado = 100.00;
+        double margemErro = 0.01;
+        assertEquals(valorEsperado, pagamento.getValorPago(), margemErro, "O valor pago deve ser o mesmo da conta, sem acréscimo");
         assertEquals(TipoPagamento.BOLETO, pagamento.getTipoPagamento(), "O tipo de pagamento deve ser BOLETO");
     }
 
