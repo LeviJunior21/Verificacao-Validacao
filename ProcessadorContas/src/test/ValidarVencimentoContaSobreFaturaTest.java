3153a9fc93c257bc649ab6c6a5832ebe6ce39858
LeviJunior21 <levi.pereira.junior@ccc.ufcg.edu.br>
2024-07-31 22:52:53 -0300

    Criando teste para checar a validação se ainda não ultrapassou o vencimento da fatura

---
+++ ProcessadorContas/src/test/ValidarVencimentoContaSobreFaturaTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import main.Fatura;
+import main.ProcessadorContas;
+import main.StatusPagamento;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Date;
+import org.junit.jupiter.api.BeforeEach;
+
+public class ValidarVencimentoContaSobreFaturaTest {
+    private ProcessadorContas processadorContas;
+
+    @BeforeEach
+    void setUp() {
+        processadorContas = new ProcessadorContas();
+    }
+
+    @Test
+    void testValidarPagamentoFaturaPaga() {
+    	// Arrange
+        Fatura fatura = new Fatura(new Date(), 100.00, "Usuario 1");
+        Double valorTotalPagar = 150.00;        
+        
+        // Act
+        processadorContas.validarPagamento(valorTotalPagar, fatura);
+        
+        // Assert
+        assertEquals(StatusPagamento.PAGA, fatura.getStatusPagamento(), "A fatura deve ser marcada como PAGA quando o valor total pago é maior ou igual ao valor da fatura");
+    }
+}
