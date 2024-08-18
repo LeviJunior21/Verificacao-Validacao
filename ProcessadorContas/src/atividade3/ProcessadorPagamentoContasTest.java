fb6084903eb9502a0c2444b4cdc10660764152ae
LeviJunior21 <levi.pereira.junior@ccc.ufcg.edu.br>
2024-08-17 21:33:34 -0300

    Teste processador pagamento boleto com limite inferior

--- ProcessadorContas/src/atividade3/ProcessadorPagamentoContasTest.java
+++ ProcessadorContas/src/atividade3/ProcessadorPagamentoContasTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import main.Conta;
 import main.Fatura;
 import main.ProcessadorContas;
+import main.StatusPagamento;
 import main.TipoPagamento;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,4 +29,17 @@ public class ProcessadorPagamentoContasTest {
         });
     }
 
+    @Test
+    public void testValorBoletoLimiteInferior1() {
+        // Arrange
+        Conta conta = new Conta("124", new Date(), 0.01, TipoPagamento.BOLETO);
+        Fatura fatura = new Fatura(new Date(), 1000.00, "Maria");
+        ProcessadorContas processador = new ProcessadorContas();
+
+        // Act
+        processador.processarContas(fatura, Arrays.asList(conta));
+
+        // Assert
+        assertEquals(StatusPagamento.PENDENTE, fatura.getStatusPagamento());
+    }
 }
