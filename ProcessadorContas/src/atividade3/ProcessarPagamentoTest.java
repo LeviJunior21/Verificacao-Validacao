0362a1e800ce8bd51ec6f50b48e94f6c82b4d256
LeviJunior21 <levi.pereira.junior@ccc.ufcg.edu.br>
2024-08-17 21:52:11 -0300

    teste: Teste para quando o valor da fatura é menor que a soma do pagamento

---
+++ ProcessadorContas/src/atividade3/ProcessarPagamentoTest.java
@@ -0,0 +1,39 @@
+package atividade3;
+
+import org.junit.jupiter.api.Test;
+
+import main.Conta;
+import main.Fatura;
+import main.ProcessadorContas;
+import main.StatusPagamento;
+import main.TipoPagamento;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Date;
+import java.util.Arrays;
+import java.util.Calendar;
+
+public class ProcessarPagamentoTest {
+
+    private Date criarData(int dia, int mes, int ano) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, dia);
+        calendar.set(Calendar.MONTH, mes - 1);
+        calendar.set(Calendar.YEAR, ano);
+        return calendar.getTime();
+    }
+
+    @Test
+    public void testValorFaturaMenorQueSomaPagamentos() {
+        // Arrange
+        Fatura fatura = new Fatura(criarData(23, 07, 2024), 999.98, "Usuario1");
+        Conta conta = new Conta("123", criarData(01, 07, 2024), 1000.00, TipoPagamento.CARTAO_CREDITO);
+        ProcessadorContas processador = new ProcessadorContas();
+
+        // Act
+        processador.processarContas(fatura, Arrays.asList(conta));
+
+        // Assert
+        assertEquals(StatusPagamento.PAGA, fatura.getStatusPagamento());
+    }
+}
